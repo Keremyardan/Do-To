baa6c83a02850523613a4526df3fd54f71125bf8
unknown <keremyrdn@hotmail.com>
2023-12-19 11:23:25 +0300

    all over

--- App.js
+++ App.js
@@ -1,78 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { FlatList, StyleSheet, View } from 'react-native';
-import AddInput from './components/AddInput';
-import { useState } from 'react';
-import AppLoading from 'expo-app-loading';
-import Header from './components/Header'
-import Empty from './components/Empty'
-import TodoList from './components/TodoList'
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
 
-const getFonts = () => {
-  Font.loadAysnc ({
-    "poppins-regular": require("./assets/fonts/Poppins/Poppins-Regular.ttf"),
-    "poppins-bold": require("./assets/fonts/Poppins/Poppins-Bold.ttf"),
-  })
-};
 
-export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-
-  const [data, setData] = useState([]);
-
-  const submitHandler = (value) => {
-    setData((prevToDo) => {
-      return[
-        {
-          value:value,
-          key:Math.random().toString(),
-        },
-        ...prevToDo,
-      ];
-    });
-  };
-
-  const deleteItem = (key) => {
-    setData((prevToDo)=> {
-      return prevToDo.filter((todo)=> todo.key != key)
-    });
-  };
-
-  if (fontsLoaded) {
-    return (
-      <View>
-        <View>
-          <StatusBar barstyle="light-content" backgroundColor='midnightblue'/>
-          <View>
-            <FlatList
-            data={data}
-            ListHeaderComponent={() =><Header/>}
-            ListEmptyComponent={()=> <Empty/>}
-            keyExtractor={(item)=>item.key}
-            renderItem={({item})=> <TodoList item={item} deleteItem={deleteItem}/>}/>
-            <View>
-              <AddInput submitHandler={submitHandler}/>
-            </View>
-          </View>
-        </View>
-      </View>
-    )
-  } else {
-    return (
-      <AppLoading
-        startAsync={getFonts}
-        onFinish={()=>{
-          setFontsLoaded(true);
-        }}
-        onError={console.warn}/>
-    )
-  }
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Do-To...</Text>
+    </View>
+  )
 }
-   
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+const styles = StyleSheet.create ({
+  container:{
+    flex:1,
+    backgroundColor:'#FFC594',
   },
+  text:{
+    textAlign:'center',
+    
+  }
 })
+
+export default App
